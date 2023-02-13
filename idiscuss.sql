fafe3f7dfafbc0ab6a2eb0c2eae48d61b5dc5afe
Jeel-Viradiya <jeelviradiya188@gmail.com>
2023-02-13 20:12:26 +0530

    fifth commit

--- idiscuss.sql
+++ idiscuss.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 19, 2023 at 04:25 PM
--- Server version: 10.4.22-MariaDB
--- PHP Version: 8.1.2
+-- Generation Time: Feb 13, 2023 at 03:40 PM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.2.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -32,7 +32,21 @@ CREATE TABLE `categories` (
   `category_name` varchar(255) NOT NULL,
   `category_description` varchar(255) NOT NULL,
   `created` datetime NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `categories`
+--
+
+INSERT INTO `categories` (`category_id`, `category_name`, `category_description`, `created`) VALUES
+(1, 'Python', 'Python is a high-level, general-purpose programming language. Its design philosophy emphasizes code readability with the use of significant indentation. Python is dynamically-typed and garbage-collected.', '2022-10-08 18:08:53'),
+(2, 'JavaScript', 'JavaScript, often abbreviated as JS, is a programming language that is one of the core technologies of the World Wide Web, alongside HTML and CSS. As of 2022, 98% of websites use JavaScript on the client side for webpage behavior, often incorporating thir', '2022-10-08 18:09:31'),
+(3, 'Django', 'Django is a free and open-source, Python-based web framework that follows the model–template–views architectural pattern. ', '2022-10-09 00:22:05'),
+(4, 'Flask', 'Flask is a micro web framework written in Python.', '2022-10-09 00:22:49'),
+(5, 'php', 'php', '2023-02-13 20:08:06'),
+(6, 'Java', 'Java', '2023-02-13 20:08:54'),
+(7, 'C & C++', 'C & C++', '2023-02-13 20:09:15'),
+(8, 'Data Structure', 'Data Structure', '2023-02-13 20:09:44');
 
 -- --------------------------------------------------------
 
@@ -41,12 +55,12 @@ CREATE TABLE `categories` (
 --
 
 CREATE TABLE `comments` (
-  `comment_id` int(11) NOT NULL,
+  `comment_id` int(8) NOT NULL,
   `comment_content` text NOT NULL,
-  `thread_id` int(11) NOT NULL,
-  `comment_by` int(11) NOT NULL,
-  `comment_time` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `thread_id` int(8) NOT NULL,
+  `comment_by` int(8) NOT NULL,
+  `comment_time` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -55,13 +69,13 @@ CREATE TABLE `comments` (
 --
 
 CREATE TABLE `threads` (
-  `thread_id` int(11) NOT NULL,
+  `thread_id` int(7) NOT NULL,
   `thread_title` varchar(255) NOT NULL,
   `thread_desc` text NOT NULL,
-  `thread_cat_id` int(11) NOT NULL,
-  `thread_user_id` int(11) NOT NULL,
+  `thread_cat_id` int(7) NOT NULL,
+  `thread_user_id` int(7) NOT NULL,
   `timestamp` datetime NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -71,10 +85,10 @@ CREATE TABLE `threads` (
 
 CREATE TABLE `users` (
   `sno` int(11) NOT NULL,
-  `user_email` varchar(255) NOT NULL,
+  `user_email` varchar(30) NOT NULL,
   `user_pass` varchar(255) NOT NULL,
   `timestamp` datetime NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Indexes for dumped tables
@@ -97,6 +111,9 @@ ALTER TABLE `comments`
 --
 ALTER TABLE `threads`
   ADD PRIMARY KEY (`thread_id`);
+ALTER TABLE `threads` ADD FULLTEXT KEY `thread_title` (`thread_title`,`thread_desc`);
+ALTER TABLE `threads` ADD FULLTEXT KEY `thread_title_2` (`thread_title`,`thread_desc`);
+ALTER TABLE `threads` ADD FULLTEXT KEY `thread_title_3` (`thread_title`,`thread_desc`);
 
 --
 -- Indexes for table `users`
@@ -112,25 +129,25 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `categories`
 --
 ALTER TABLE `categories`
-  MODIFY `category_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `category_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `comments`
 --
 ALTER TABLE `comments`
-  MODIFY `comment_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `comment_id` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `threads`
 --
 ALTER TABLE `threads`
-  MODIFY `thread_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `thread_id` int(7) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `sno` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `sno` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
